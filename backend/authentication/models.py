17398eba0cf93607e21f33d317f1f8a494d679ac
Amir <am17iryaghoobi@gmail.com>
2025-04-30 15:38:32 +0330

    add super admin role and create a serializer for RegularCustomer Registration

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     class RoleStatus(models.TextChoices):
+        SUPER_ADMIN = "super_admin", "ادمین ارشد"
         ADMIN = "admin", "ادمین"
         REGULAR = "regular", "مشتری معمولی"
         VIP = "vip", "مشتری ویژه"
@@ -16,23 +17,27 @@ class User(AbstractUser):
         verbose_name="تصویر پروفایل",
     )
     role = models.CharField(
-        max_length=10,
+        max_length=15,
         choices=RoleStatus.choices,
         default=RoleStatus.REGULAR,
         verbose_name="نقش کاربر",
     )
 
     @property
+    def is_super_admin(self):
+        return self.role == self.RoleStatus.SUPER_ADMIN
+
+    @property
     def is_admin(self):
-        return self.role == self.ADMIN
+        return self.role == self.RoleStatus.ADMIN
 
     @property
     def is_vip(self):
-        return self.role == self.VIP
+        return self.role == self.RoleStatus.VIP
 
     @property
     def is_regular(self):
-        return self.role == self.REGULAR
+        return self.role == self.RoleStatus.REGULAR
 
     def __str__(self):
         return f"{self.username} - {self.phone}"
---
+++ backend/authentication/serializers.py
@@ -0,0 +1,23 @@
+from .models import *
+from rest_framework import serializers
+
+
+class RegularCustomerRegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["username", "phone", "email", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def validate_phone(self, value):
+        if not value.isdigit() or not value.startswith("09") or len(value) != 11:
+            raise serializers.ValidationError("شماره تلفن وارد شده نامعتبر است.")
+
+        if User.objects.filter(phone=value).exists():
+            raise serializers.ValidationError("شماره تلفن وارد شده قبلاً ثبت شده است.")
+
+        return value
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("ایمیل وارد شده قبلا ثبت شده است.")
+        return value
