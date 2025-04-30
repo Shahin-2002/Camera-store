7c7ff124a21a6f74e9d1824dc7e5738d393d1bf2
Amir <am17iryaghoobi@gmail.com>
2025-04-30 03:35:06 +0330

    create user model

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    phone = models.CharField(max_length=11, verbose_name="شماره تلفن")
+    profile_picture = models.ImageField(
+        upload_to="profile_pictures/", verbose_name="تصویر پروفایل"
+    )
+
+    def __str__(self):
+        return f"{self.username} - {self.phone}"
