1db0dce6231fb28d3a9ac7095df1e1edec50596f
Amir <am17iryaghoobi@gmail.com>
2025-05-01 16:49:04 +0330

    add internal cache settings for backend

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -171,3 +171,11 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "LOCATION": "register-verification",
+    }
+}
