0e4fae4e88e28f010c943e48849c5caac70475c8
Amir <am17iryaghoobi@gmail.com>
2025-05-01 16:57:16 +0330

    cache settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -164,10 +164,18 @@ SIMPLE_JWT = {
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
 }
 
-
+# Email configuration for sending emails via Gmail's SMTP server
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = "tiamtestblog@gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+# Cache configuration for storing temporary data like register verification
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "LOCATION": "register-verification",
+    }
+}
