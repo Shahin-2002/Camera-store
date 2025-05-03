ab7a75622c1ab79b89c398bceb915790b3cfed0d
Amir <am17iryaghoobi@gmail.com>
2025-05-04 00:51:59 +0330

    change access token life time

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -160,7 +160,7 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "AUTH_COOKIE": "access_token",
     "AUTH_COOKIE_SECURE": False,
