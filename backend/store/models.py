5d672b8dc632ef062ad0247fc0dc93018fa7c374
Amir <am17iryaghoobi@gmail.com>
2025-05-07 15:52:48 +0330

    feat(store): create category model

--- backend/store/models.py
+++ backend/store/models.py
@@ -1,1 +1,20 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
+
+
+class Category(MPTTModel):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    parent = TreeForeignKey(
+        to="self",
+        null=True,
+        blank=True,
+        related_name="children",
+        on_delete=models.CASCADE,
+    )
+
+    class MPTTMeta:
+        order_insertion_by = ["name"]
+
+    def __str__(self):
+        return self.name
