4dee74aac13dd0874d7686ce6827c651a6c8a66a
Amir <am17iryaghoobi@gmail.com>
2025-05-07 19:40:55 +0330

    add API documentation

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "authentication",
     "store",
     "mptt",
+    "drf_spectacular",
 ]
 
 MIDDLEWARE = [
@@ -158,6 +159,16 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.SessionAuthentication",
     ),
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+# Configuration for drf-spectacular, used for generating API documentation
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Camera Store API",
+    "DESCRIPTION": "API documentation for the Camera Store project",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+    # Additional settings can be added here
 }
 
 
