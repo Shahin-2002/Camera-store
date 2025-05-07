b31c7bdb07e5622c05f643b8225799bd143ba8b1
Amir <am17iryaghoobi@gmail.com>
2025-05-07 17:27:17 +0330

    feat(store): create Product model

--- backend/store/models.py
+++ backend/store/models.py
@@ -18,3 +18,36 @@ class Category(MPTTModel):
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        to=Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name="دسته بندی",
+    )
+    name = models.CharField(max_length=100, verbose_name="نام محصول")
+    slug = models.SlugField(unique=True, verbose_name="اسلاگ")
+    description = models.TextField(max_length=1200, verbose_name="توضیحات")
+    stock = models.PositiveIntegerField(default=0, verbose_name="موجودی")
+    price = models.PositiveIntegerField(default=0, verbose_name="قیمت محصول")
+    off = models.PositiveIntegerField(default=0, verbose_name="درصد تخفیف")
+    new_price = models.PositiveIntegerField(default=0, verbose_name="قیمت با تخفیف")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="تاریخ ایجاد")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="تاریخ ویرایش")
+
+    class Meta:
+        ordering = ["-created_at"]
+        indexes = [
+            models.Index(fields=["id", "slug"]),
+            models.Index(fields=["name"]),
+            models.Index(fields=["-created_at"]),
+        ]
+
+        verbose_name = "محصول"
+        verbose_name_plural = "محصولات"
+
+    def __str__(self):
+        return self.name
