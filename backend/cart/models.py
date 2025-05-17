e377b098813e69d6bd8b0307d8e017f2706484e0
Amir <am17iryaghoobi@gmail.com>
2025-05-17 15:46:10 +0330

    feat(cart): create Cart model

--- backend/cart/models.py
+++ backend/cart/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from authentication.models import User
 
-# Create your models here.
+
+class Cart(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE, related_name="cart")
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Cart for {self.user.username}"
