40513574cbf8fe0db3cc7cdbf65b49416121dcd8
Amir <am17iryaghoobi@gmail.com>
2025-05-25 04:36:31 +0330

    create Order and OrderItem models

--- backend/orders/models.py
+++ backend/orders/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from authentication.models import User
+from cart.models import Cart, CartItem
+import uuid
+from store.models import Product
 
-# Create your models here.
+
+class Order(models.Model):
+    class OrderStatus(models.TextChoices):
+        PENDING = "pending", "در حال بررسی"
+        PAID = "paid", "پرداخت شده"
+        SHIPPED = "shipped", "ارسال شده"
+        DELIVERED = "delivered", "تحویل داده شده"
+        CANCELLED = "cancelled", "لغو شده"
+
+    order_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
+    cart = models.OneToOneField(Cart, on_delete=models.CASCADE, related_name="orders")
+    status = models.CharField(
+        max_length=20, choices=OrderStatus.choices, default=OrderStatus.PENDING
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    province = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    address = models.TextField()
+    postal_code = models.CharField(max_length=10)
+
+    is_paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Order {self.id} - {self.user.username}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="order_items"
+    )
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} in order {self.order.id}"
